d2a2acfbfbe3386e7422c83e530437ae9420e04e
yohkawano <ykawano@reitaku-u.ac.jp>
2024-02-14 18:13:14 +0900

    interactive charts

--- buildinguse_change/js/map.js
+++ buildinguse_change/js/map.js
@@ -5,53 +5,10 @@ var extrusionHeight = 10; // Default extrusion height
 var mapStyle = 'mapbox://styles/mapbox/satellite-streets-v11'; // Default map style
 var selectedyear = '2016'; // Default selected year
 
-const map = new mapboxgl.Map({
-	container: 'map',
-	// style: mapStyle,
-	center: [139.6917, 35.6895],
-	zoom: 10,
-	// pitch: 45, // Set the pitch angle to make it a 3D map
-	// bearing: -17.6 // Set the bearing angle for a better perspective
-});
+var max_bin_val = 20;
 
-function changeMapStyle(style) {
-	map.setStyle(style);
-}
-
-document.getElementById('dropdown').addEventListener('change', function() {
-	selectedvar = this.value;
-	updateExtrusionLayer_diff();
-	labelMinMax();
-	addChart();
-});
-
-document.getElementById('extrusion-slider').addEventListener('input', function() {
-	max_bin_val = parseInt(this.value);
-	updateExtrusionLayer_diff();
-	updateLegend();
-});
-
-document.getElementById('style-selector').addEventListener('change', function() {
-	map.setStyle(this.value);
-	
-	
-});
-
-// create a function to change the year
-function changeYear(year) {
-	selectedyear = year;
-	console.log(selectedyear);
-	// change the class of the year buttons from active to inactive according to the selected year
-	if (selectedyear == '2011') {
-		document.getElementById('button-2011').className = 'active';
-		document.getElementById('button-2016').className = 'inactive';
-	} else {
-		document.getElementById('button-2011').className = 'inactive';
-		document.getElementById('button-2016').className = 'active';
-	}
-	// update the extrusion layer
-	updateExtrusionLayer();
-}
+var minPopup = document.createElement('div');
+var maxPopup = document.createElement('div');
 
 var popup = new mapboxgl.Popup({
 	className: 'popup',
@@ -61,19 +18,58 @@ var popup = new mapboxgl.Popup({
 
 var colors = ['rgba(165,0,38,0.8)', 'rgba(248,139,81,0.8)', 'rgba(253,254,186,0.8)', 'rgba(113,193,100,0.8)', 'rgba(0,104,55,0.8)'];
 
-function updateLegend(){
+// create a line array
+var lineArray = [];
+// create a key_code array
+var keyArray = [];
 
-	// getdocumentid legend1, legend2 and legend3, and update their background color
-	document.getElementById('legend1').style.backgroundColor = colors[0];
-	document.getElementById('legend2').style.backgroundColor = colors[2];
-	document.getElementById('legend3').style.backgroundColor = colors[4];
-	// set the text of the legend to the max_bin_val
-	document.getElementById('legend1').innerHTML = -max_bin_val + ' (decrease)';
-	document.getElementById('legend2').innerHTML = 0 + ' (no change)';
-	document.getElementById('legend3').innerHTML = max_bin_val + ' (increase)';
+const map = new mapboxgl.Map({
+	container: 'map',
+	// style: mapStyle,
+	center: [139.6917, 35.6895],
+	zoom: 10,
+	// pitch: 45, // Set the pitch angle to make it a 3D map
+	// bearing: -17.6 // Set the bearing angle for a better perspective
+});
 
 
-}
+
+document.getElementById('dropdown').addEventListener('change', function() {
+	selectedvar = this.value;
+	updateExtrusionLayer_diff();
+	labelMinMax();
+	makeChart();
+});
+
+document.getElementById('extrusion-slider').addEventListener('input', function() {
+	max_bin_val = parseInt(this.value);
+	updateExtrusionLayer_diff();
+	updateLegend();
+	makeChart();
+});
+
+var showTop = true
+
+// Add event listener to radio buttons
+document.querySelectorAll('input[type="radio"]').forEach(function(radio) {
+    radio.addEventListener('change', function() {
+		// Update variable based on selected option
+		showTop = this.value === 'true';
+		makeChart();						
+		// Log the variable value
+		console.log('showTop:', showTop);
+    });
+  });
+
+// document.getElementById('style-selector').addEventListener('change', function() {
+// 	map.setStyle(this.value);
+	
+// });
+
+
+// ------------------------------------------------
+// Load the map
+// ------------------------------------------------
 
 map.on('load', function () {
 	console.log('loading map...');
@@ -83,7 +79,7 @@ map.on('load', function () {
 	map.addSource('my-data', {
 		type: 'geojson',
 		// data: 'data/mesh2011_2016.geojson'
-		data: 'data/mesh2011_2016_diff.geojson'
+		data: 'data/mesh2011_2016_master.geojson'
 		// data: 'data/mesh2011.geojson'
 	});
 	
@@ -150,7 +146,7 @@ map.on('load', function () {
 	updateExtrusionLayer_diff();
 	// execute this function 1 second after the map is loaded
 	setTimeout(labelMinMax, 1000);
-	setTimeout(addChart, 1000);
+	setTimeout(makeChart, 1000);
 	// labelMinMax();
 
 
@@ -172,10 +168,9 @@ function updateExtrusionLayer() {
 
 }
 
-var max_bin_val = 20;
-
-var minPopup = document.createElement('div');
-var maxPopup = document.createElement('div');
+function changeMapStyle(style) {
+	map.setStyle(style);
+}
 
 function labelMinMax() {
 
@@ -264,6 +259,7 @@ function labelMinMax() {
 	  
 }
 
+
 function updateExtrusionLayer_diff() {
 
 	console.log('updateExtrusionLayer_diff');
@@ -284,81 +280,149 @@ function updateExtrusionLayer_diff() {
 
 }
 
-function makeBox(num) {
-	var box = '';
-
-	if (num === 0) {
-		box += '<span style="background-color:gray;border:1px solid white;width:25px;height:25px;font-size:0.6em;color:white;display:inline-block;line-height:25px;text-align:center;"></span>';
-	} else {
-		for (var i = 0; i < num; i++) {
-			box += '<span style="background-color:red;border:1px solid white;width:25px;height:25px;font-size:0.6em;color:white;display:inline-block;line-height:25px;text-align:center;">50</span>';
-		}
-	}
-	return box;
-}
-
 
 // ------------------------------------------------
 // function to access info-panel and add a chart
 // ------------------------------------------------
 
-// create a line array
-var lineArray = [];
-// create a key_code array
-var key_codeArray = [];
+function makeChart2() {
+	// English
+// Sample data
 
-function addChart() {
+
+
+var data = [{
+	x: [-2, 2, 3, 4],
+	y: ['A', 'B', 'C', 'D'],
+	type: 'bar',
+	marker: {
+	  color: [-2,-1,0,], // Values for color scale
+	//   colorscale: 'RdBu', // Diverging color scale
+	  colorscale: [[0, colors[0]], [0.5, colors[2]], [1, colors[4]]], // Custom colorscale
+	  cmin: -2, // Minimum value for color scale
+	  cmax: 2, // Maximum value for color scale
+	  colorbar: {
+		title: 'Color Scale' // Title for color bar
+	  }
+	},
+	orientation: 'h'
+  }];
+  
+  // Layout options
+  var layout = {
+	title: 'Horizontal Bar Chart with Diverging Color Scale',
+	xaxis: { title: 'Value' },
+	yaxis: { title: 'Category' }
+  };
+  
+  // Plot the chart
+  Plotly.newPlot('info-panel', data, layout);
+}
+
+// ------------------------------------------------
+// function to access info-panel and add a chart
+// ------------------------------------------------
+
+function makeChart() {
 	// clear the arrays
 	lineArray = [];
-	key_codeArray = [];
+	keyArray = [];
+	// showTop = True;
 	
-	console.log('addChart...');
+
+	// get the info-panel and clear it
 	var chart = document.getElementById('info-panel');
 	chart.innerHTML = ''
 
-
+	// get the features from the map
 	const features = map.queryRenderedFeatures({ layers: ['mesh-borders'] });
-
-
 
 	// sort the elements by greatest to smallest
 	features.sort((a, b) => b.properties[selectedvar+'_diff'] - a.properties[selectedvar+'_diff']);
 
-
 	// loop through every element in features
 	features.forEach(element => {
-		
-		// draw a line for each element with the length being the variable value
+		// add values to arrays
 		const lineLength = element.properties[selectedvar+'_diff'];
-		const key_code = element.properties['KEY_CODE'];
-
-		// add length to linearray
+		const key_code = element.properties['KEY_CODE_left'];
 		lineArray.push(lineLength);
-		key_codeArray.push(key_code);
+		keyArray.push(key_code);
 	});
 
-	// find the value of the 10th element, and slice the array to only include all numeric values that are larger than the 10th element
-	var tenth = lineArray[200];
-	lineArray = lineArray.filter(value => value >= tenth);
-	console.log(lineArray);
+	// find the value of the nth element, and slice the array to only include all numeric values that are larger than the 10th element
+	// slice the lineArray to the top 200 elements
+	var n = 100;
+	top_lineArray = lineArray.slice(0, n);
+	top_keyArray = keyArray.slice(0, n);
+	bottom_lineArray = lineArray.slice(-n);
+	bottom_keyArray = keyArray.slice(-n);
+
+	
+	// var tenth = lineArray[200];
+	// lineArray = lineArray.filter(value => value >= tenth);
+	// console.log(lineArray);
 	// reverse the array so that the largest value is at the top
-	lineArray.reverse()
-	// now reverse the key_codeArray so that the key_code matches the lineArray, then slice the array to the same number of elements as lineArray
-	key_codeArray.reverse()
-	key_codeArray = key_codeArray.slice(0, lineArray.length);
-	// now reverse it again
-	key_codeArray.reverse()
+	if (showTop) {
+		console.log('showTop')
+		lineArray=top_lineArray.reverse();
+		keyArray=top_keyArray.reverse()
+		// lineArray=top_lineArray.reverse();
+		// keyArray=top_keyArray.reverse()
+	}
+	else {
+		console.log('showBottom')
+		lineArray=bottom_lineArray.reverse();
+		keyArray=bottom_keyArray.reverse()
+	}
+
+	
+
+	// make a copy of lineArray
+	var lineArrayColors = lineArray.slice();
+
+	// go over each value in lineArray, and for any value greater than 20, set it to 20, and for any value less than -20, set it to -20
+	for (var i = 0; i < lineArrayColors.length; i++) {
+		if (lineArrayColors[i] > max_bin_val) {
+			lineArrayColors[i] = max_bin_val;
+		}
+		if (lineArrayColors[i] < -max_bin_val) {
+			lineArrayColors[i] = -max_bin_val;
+		}
+	}
 
 	var data = [{
 		type: 'bar',
 		x: lineArray,
-		// y: key_codeArray,
-		// y: ['giraffes', 'orangutans', 'monkeys'],
 		orientation: 'h',
-		hovertemplate: 'Value: %{x} key_code: %{y} %{text}',
-	  }];
+		hovertemplate: 'Building change: %{x}',
+		marker: {
+			color: lineArrayColors, // Values for color scale
+			colorscale: [[0, colors[0]], [0.5, colors[2]], [1, colors[4]]], // Custom colorscale
+			cmin: -max_bin_val, // Minimum value for color scale
+			cmax: max_bin_val, // Maximum value for color scale
+			colorbar: {
+			  title: 'Color Scale' // Title for color bar
+			}
+		  },
 	  
-	  Plotly.newPlot('info-panel', data);
+
+
+
+
+
+	}];
+		
+	// Layout options
+	var layout = {
+		xaxis: {
+			//title: 'X Axis Title' // Add X axis title if needed
+		},
+		yaxis: {
+			showticklabels: false // Hide y-axis tick labels
+		}
+	};
+
+	Plotly.newPlot('info-panel', data, layout, {displayModeBar: false});
 	  
 
 	// Add event listener to capture hover event
@@ -370,33 +434,64 @@ function addChart() {
 		// Log x and y values to the console
 		console.log('Hovered X:', xValue);
 		console.log('Hovered Y:', yValue);
-		console.log('Hovered keycode:', key_codeArray[yValue]);
+		console.log('Hovered keycode:', keyArray[yValue]);
 		highlightFeature(yValue);
 
 	});
 
-	// // draw a line for each element in lineArray
-	// lineArray.forEach(length => {
-
-	//     drawLine(length);
-	// });
-
-	// // function to draw a line
-	// function drawLine(length) {
-	// // code to draw a line with the given length
-	//     chart.innerHTML += length + '<br>';
-	// }
-
-	
 }
 
 // function to highlight a feature from mesh-borders by KEY_CODE
 function highlightFeature(pos) {
 
 	// filter by key_code and paint the feature with a different color
-	map.setFilter('mesh-borders', ['==', 'KEY_CODE', key_codeArray[pos]]);
+	map.setFilter('mesh-borders', ['==', 'KEY_CODE_left', keyArray[pos]]);
 	map.setPaintProperty('mesh-borders', 'line-color', 'red');
 	map.setPaintProperty('mesh-borders', 'line-width', 4);
 
 	
+}
+
+function updateLegend(){
+
+	// getdocumentid legend1, legend2 and legend3, and update their background color
+	document.getElementById('legend1').style.backgroundColor = colors[0];
+	document.getElementById('legend2').style.backgroundColor = colors[2];
+	document.getElementById('legend3').style.backgroundColor = colors[4];
+	// set the text of the legend to the max_bin_val
+	document.getElementById('legend1').innerHTML = '⬇︎'+ -max_bin_val + ' (decrease)';
+	document.getElementById('legend2').innerHTML = 0 + ' (no change)';
+	document.getElementById('legend3').innerHTML = '⬆︎'+max_bin_val + ' (increase)';
+
+
+}
+
+// create a function to change the year
+function changeYear(year) {
+	selectedyear = year;
+	console.log(selectedyear);
+	// change the class of the year buttons from active to inactive according to the selected year
+	if (selectedyear == '2011') {
+		document.getElementById('button-2011').className = 'active';
+		document.getElementById('button-2016').className = 'inactive';
+	} else {
+		document.getElementById('button-2011').className = 'inactive';
+		document.getElementById('button-2016').className = 'active';
+	}
+	// update the extrusion layer
+	updateExtrusionLayer();
+}
+
+
+function makeBox(num) {
+	var box = '';
+
+	if (num === 0) {
+		box += '<span style="background-color:gray;border:1px solid white;width:25px;height:25px;font-size:0.6em;color:white;display:inline-block;line-height:25px;text-align:center;"></span>';
+	} else {
+		for (var i = 0; i < num; i++) {
+			box += '<span style="background-color:red;border:1px solid white;width:25px;height:25px;font-size:0.6em;color:white;display:inline-block;line-height:25px;text-align:center;">50</span>';
+		}
+	}
+	return box;
 }
